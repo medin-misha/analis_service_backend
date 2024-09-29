9829ac02b5052a05e11b2ef237ec209d25e66baa
misha <medinskijmisa228@gmail.com>
2024-09-29 15:48:13 +0300

    add user model

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -2,3 +2,4 @@ __all__ = "Base", "settings"
 
 from .config import settings
 from .models.base import Base
+from .models.user import User
---
+++ app/core/models/user.py
@@ -0,0 +1,9 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+from .base import Base
+
+class User(Base):
+    name: Mapped[str]
+    age: Mapped[int]
+    weight: Mapped[int]
+    gender: Mapped[bool]
