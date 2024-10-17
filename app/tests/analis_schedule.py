537782f7b082eafac6a917a65b05274e3c4ac56a
misha <medinskijmisa228@gmail.com>
2024-10-17 22:16:36 +0300

    update analis schedule tests

--- app/tests/analis_schedule.py
+++ app/tests/analis_schedule.py
@@ -1,1 +1,48 @@
-from .fixtures import database, client
+from .fixtures import database_schedule, client
+from httpx import Response
+
+
+def test_analis_schedule(database_schedule):
+    client = database_schedule[0]
+    user = database_schedule[1]
+    analis = database_schedule[2]
+
+    user_and_analis_id: dict = {"user_id": user["id"], "analis_id": analis["id"]}
+    response: Response = client.post("/schedule/", json=user_and_analis_id)
+    assert response.status_code == 200
+    assert response.headers.get("content-type") == "image/png"
+
+
+def test_analis_schedule_not_found_user(database_schedule):
+    client = database_schedule[0]
+    user_and_analis_id: dict = {"user_id": 12, "analis_id": 1}
+    response: Response = client.post("/schedule/", json=user_and_analis_id)
+    assert response.status_code == 404
+
+
+def test_analis_schedule_not_found_analis(database_schedule):
+    client = database_schedule[0]
+    user_and_analis_id: dict = {"user_id": 1, "analis_id": 21}
+    response: Response = client.post("/schedule/", json=user_and_analis_id)
+    assert response.status_code == 404
+
+
+def test_analis_schedule_bad_data(database_schedule):
+    user: dict = database_schedule[1]
+    analis: dict = database_schedule[2]
+
+    client = database_schedule[0]
+    user_and_analis_id: dict = {"user_id": user["id"], "analis_id": analis["id"]}
+    bad_analis_value_data: dict = {
+        "user_id": user["id"],
+        "analis_id": analis["id"],
+        "value": "пиздец в место значения",
+        "date": "2024-10-09",
+    }
+    analis_value_create_response: Response = client.post(
+        "analis/value/", json=bad_analis_value_data
+    )
+    response: Response = client.post("/schedule/", json=user_and_analis_id)
+
+    assert analis_value_create_response.status_code == 200
+    assert response.status_code == 409
--- app/tests/fixtures.py
+++ app/tests/fixtures.py
@@ -16,7 +16,12 @@ def client():
 def database(client):
     user_data: dict = {"name": "misha", "age": 16, "weight": 65, "gender": True}
     analis_data: dict = {"name": "ПСА (PSA) - коефіцієнт", "unit": "%"}
-    analis_value_data: dict = {"user_id": 1, "analis_id": 1, "value": "50", "date": "2024-10-19"}
+    analis_value_data: dict = {
+        "user_id": 1,
+        "analis_id": 1,
+        "value": "50",
+        "date": "2024-10-19",
+    }
     analis_standart_data: dict = {
         "analis_id": 1,
         "gender": True,
@@ -29,9 +34,9 @@ def database(client):
 
     user_create_response: Response = client.post("/users", json=user_data)
     analis_create_response: Response = client.post("/analis", json=analis_data)
-    analis_standart_create_response: Response = client.post(
-        "/analis/standart/", json=analis_standart_data
-    )
+    # analis_standart_create_response: Response = client.post(
+    #     "/analis/standart/", json=analis_standart_data
+    # )
     analis_value_create_response: Response = client.post(
         "analis/value/", json=analis_value_data
     )
@@ -41,3 +46,80 @@ def database(client):
     # assert analis_standart_create_response.status_code == 200
     assert analis_value_create_response.status_code == 200
     return client
+
+
+@pytest.fixture
+def database_schedule(client):
+    user_data: dict = {"name": "misha", "age": 16, "weight": 65, "gender": True}
+    analis_data: dict = {"name": "ПСА (PSA) - коефіцієнт", "unit": "%"}
+
+    user_create_response: Response = client.post("/users", json=user_data)
+    analis_create_response: Response = client.post("/analis", json=analis_data)
+
+    assert user_create_response.status_code == 200
+    assert analis_create_response.status_code == 200
+
+    analis_values_data: list[dict] = [
+        {
+            "user_id": user_create_response.json().get("id"),
+            "analis_id": analis_create_response.json().get("id"),
+            "value": "50",
+            "date": "2024-10-01",
+        },
+        {
+            "user_id": user_create_response.json().get("id"),
+            "analis_id": analis_create_response.json().get("id"),
+            "value": "51",
+            "date": "2024-10-02",
+        },
+        {
+            "user_id": user_create_response.json().get("id"),
+            "analis_id": analis_create_response.json().get("id"),
+            "value": "53",
+            "date": "2024-10-03",
+        },
+        {
+            "user_id": user_create_response.json().get("id"),
+            "analis_id": analis_create_response.json().get("id"),
+            "value": "45",
+            "date": "2024-10-04",
+        },
+        {
+            "user_id": user_create_response.json().get("id"),
+            "analis_id": analis_create_response.json().get("id"),
+            "value": "48",
+            "date": "2024-10-05",
+        },
+        {
+            "user_id": user_create_response.json().get("id"),
+            "analis_id": analis_create_response.json().get("id"),
+            "value": "55",
+            "date": "2024-10-06",
+        },
+        {
+            "user_id": user_create_response.json().get("id"),
+            "analis_id": analis_create_response.json().get("id"),
+            "value": "43",
+            "date": "2024-10-07",
+        },
+        {
+            "user_id": user_create_response.json().get("id"),
+            "analis_id": analis_create_response.json().get("id"),
+            "value": "45",
+            "date": "2024-10-08",
+        },
+        {
+            "user_id": user_create_response.json().get("id"),
+            "analis_id": analis_create_response.json().get("id"),
+            "value": "50",
+            "date": "2024-10-09",
+        },
+    ]
+
+    for analis_value_data in analis_values_data:
+        analis_value_create_response: Response = client.post(
+            "analis/value/", json=analis_value_data
+        )
+        assert analis_value_create_response.status_code == 200
+
+    return (client, user_create_response.json(), analis_create_response.json())
